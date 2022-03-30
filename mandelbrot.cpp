74d09dcede7abc9fb3ea38f9f14a442019a30905
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2022-03-29 19:39:35 -0600

    Updates!

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -11,36 +11,30 @@ using namespace std;
 int iterations(complex<double> c) {
 	complex<double> z(0, 0);
 	int count = 0;
-	while (abs(z) < 2 && count < 80) {
-		z = (z * z) + c;
+	while (abs(z) < 2 && count < 255) {
+		z = (pow(z, 2)) + c;
 		count += 1;
 	}
 	return count;
 }
 
 
-void fill_array(sf::VertexArray& set, float scale, float width, float height, sf::Vector2f mouse, sf::Vector2f &last_shift, double &last_scale, int thread, int max_threads) {
+void fill_array(sf::VertexArray& set, float scale, float width, float height, sf::Vector2f mouse, sf::Vector2f &last_shift, long double &last_scale, int thread, int max_threads) {
 	sf::Vector2f shift;
-	double horizontalRes;
-	double verticalRes;
-	double horizontalStart;
-	double horizontalEnd;
-	double verticalStart;
-	double verticalEnd;
+	long double horizontalStart;
+	long double horizontalEnd;
+	long double verticalStart;
+	long double verticalEnd;
 	sf::Vector2f origin((width / 2), (height / 2));
-	double start = (-2 / 1);
-	double end = (2 / 1);
-	double scaleStart = (-2 / scale);
-	double scaleEnd = (2 / scale);
-	double horizontalSize = (width / (abs(scaleStart) + abs(scaleEnd)));
-	double verticalSize = (height / (abs(scaleStart) + abs(scaleEnd)));
+	long double scaleStart = (-2 / scale);
+	long double scaleEnd = (2 / scale);
+	long double horizontalSize = (width / (abs(scaleStart) + abs(scaleEnd)));
+	long double verticalSize = (height / (abs(scaleStart) + abs(scaleEnd)));
+	long double horizontalRes = (abs(scaleStart) + abs(scaleEnd)) / width * 1;
+	long double verticalRes = (abs(scaleStart) + abs(scaleEnd)) / height * 1;
+	cout << scale << endl;
 
 	shift = origin + ((origin - mouse) * scale);
-
-	horizontalSize = (width / (abs(scaleStart) + abs(scaleEnd)));
-	verticalSize = (height / (abs(scaleStart) + abs(scaleEnd)));
-	horizontalRes = (abs(scaleStart) + abs(scaleEnd)) / width * 1;
-	verticalRes = (abs(scaleStart) + abs(scaleEnd)) / height * 1;
 
 	horizontalStart = scaleStart + ((origin.x - shift.x) / horizontalSize);
 	horizontalEnd = scaleEnd + ((origin.x - shift.x) / horizontalSize);
@@ -49,9 +43,9 @@ void fill_array(sf::VertexArray& set, float scale, float width, float height, sf
 
 	sf::Vector2f position;
 	int num;
-	for (double t = horizontalStart; t < horizontalEnd; t += horizontalRes) {
+	for (long double t = horizontalStart; t < horizontalEnd; t += horizontalRes) {
 
-		for (double m = verticalStart; m < verticalEnd; m += verticalRes) {
+		for (long double m = verticalStart; m < verticalEnd; m += verticalRes) {
 
 			position = sf::Vector2f(t * horizontalSize, m * verticalSize) + shift;
 
@@ -63,25 +57,14 @@ void fill_array(sf::VertexArray& set, float scale, float width, float height, sf
 
 					num = iterations(c);
 
-					if (num < 20) {
-						set.append(sf::Vertex(position, sf::Color(num * 8, num * 6, num * 12)));
-					}
-					else if (num < 40) {
-						set.append(sf::Vertex(position, sf::Color(num * 2, num / 2, num * 6)));
-					}
-					else if (num == 80) {
-						set.append(sf::Vertex(position, sf::Color::White));
-					}
-					else {
-						set.append(sf::Vertex(position, sf::Color(num * 3, num / 2, num * 2)));
-					}
+					set.append(sf::Vertex(position, sf::Color(num * 8, num * 6, num * 12)));
 				}
 			}
 		}
 	}
 }
 
-void make_set(list<sf::VertexArray> &sets, list<sf::VertexArray>::iterator setIter, list<thread> &active_threads, int max_threads, double scale, int window_width, int window_height, sf::Vector2f mouse, sf::Vector2f &last_shift, double &last_scale) {
+void make_set(list<sf::VertexArray>& sets, list<sf::VertexArray>::iterator setIter, list<thread>& active_threads, int max_threads, long double scale, int window_width, int window_height, sf::Vector2f mouse, sf::Vector2f& last_shift, long double& last_scale) {
 	int i = 0;
 	for (setIter = sets.begin(); setIter != sets.end(); setIter++) {
 		(*setIter).clear();
@@ -100,9 +83,9 @@ int main() {
 
 	//VARIABLES-------------------------------------------------------------
 	sf::Color bgColor = sf::Color(8, 6, 12);
-	double scale = 1;
+	long double scale = 1;
 	sf::Vector2f last_shift;
-	double last_scale = 0;
+	long double last_scale = 0;
 	sf::Event event;
 	list<thread> active_threads;
 	int max_threads = 12;
@@ -141,7 +124,9 @@ int main() {
 					scale /= 2;
 				}
 				make_set(sets, setIter, active_threads, max_threads, scale, window.getSize().x, window.getSize().y, sf::Vector2f(event.mouseWheelScroll.x, event.mouseWheelScroll.y), last_shift, last_scale);
-				//cout << event.mouseWheelScroll.x << ", " << event.mouseWheelScroll.y << endl; 60.5, 495.5
+				//cout << event.mouseWheelScroll.x << ", " << event.mouseWheelScroll.y << endl;
+				//60.54, 495.55
+				//event.mouseWheelScroll.x, event.mouseWheelScroll.y
 			}
 		}
 		for (auto& th : active_threads) {
@@ -154,7 +139,7 @@ int main() {
 			totalPoints += (*setIter).getVertexCount();
 			window.draw(*setIter);
 		}
-		cout << totalPoints << endl;
+		//cout << totalPoints << endl;
 		totalPoints = 0;
 		window.draw(dot);
 		window.display();
